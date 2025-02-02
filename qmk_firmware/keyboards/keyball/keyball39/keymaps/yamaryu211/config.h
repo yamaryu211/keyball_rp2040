2db8cf07bfb2c3091821a4c0001d54888eafcda9
yamaryu211 <yamaryu380@gmail.com>
2025-02-02 18:36:25 +0800

    mod: QUICK_TAP_TERM 150に設定

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 180
 #define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 150
 #define QUICK_TAP_TERM_PER_KEY
 #define AUTO_MOUSE_TIME 450  /* マウスが止まってから元のレイヤーに戻るまでの時間(ms) */
 // for CAPS WORD
