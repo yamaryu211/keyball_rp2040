ea92f70f9bb8f5ae1c866c289e9af248f3f025ae
yamaryu211 <yamaryu380@gmail.com>
2025-02-07 16:15:25 +0800

    fix: 修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
@@ -251,18 +251,20 @@ color.h
 // clang-format on
 
 // デフォルトレイヤーの切り替え
-switch (detected_host_os()) {
-  case OS_WINDOWS:
-  case OS_LINUX:
-    default_layer_set(1UL<<_WINDOWS);
-    break;
-  case OS_MACOS:
-  case OS_IOS:
-    default_layer_set(1UL<<_MAC);
-    break;
-  default:
-    default_layer_set(1UL<<_WINDOWS);
-    break;
+void switch_default_layer(void) {
+  switch (detected_host_os()) {
+    case OS_WINDOWS:
+    case OS_LINUX:
+      default_layer_set(1UL<<_WINDOWS);
+      break;
+    case OS_MACOS:
+    case OS_IOS:
+      default_layer_set(1UL<<_MAC);
+      break;
+    default:
+      default_layer_set(1UL<<_WINDOWS);
+      break;
+  }
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
