85a6053d2669cfe36923b9eecd6bdf3d9eaa354f
yamaryu211 <yamaryu380@gmail.com>
2025-02-24 06:39:58 +0800

    設定変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
@@ -41,7 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000 // マウスレイヤーを維持する時間を30秒に設定
 
 #define TAPPING_TERM 180
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
 // タップ→ホールドしたときにタップしたキーコードを連続送信するための待受時間
 #define QUICK_TAP_TERM 150
 #define QUICK_TAP_TERM_PER_KEY
