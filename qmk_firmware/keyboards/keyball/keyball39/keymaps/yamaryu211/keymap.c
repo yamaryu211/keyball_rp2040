e643b024fa0579ed848da21bd28c3ffe2b51f810
yamaryu211 <yamaryu380@gmail.com>
2025-02-23 19:16:44 +0800

    fix: キーコード修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/keymap.c
@@ -627,7 +627,7 @@ void dance_minus_finished(tap_dance_state_t *state, void *user_data) {
       register_code(KC_EQUAL);
       break;
     case TD_DOUBLE_HOLD:
-      register_code(KC_PLUS);
+      register_code(KC_PPLS);
       break;
     default:
       break;
@@ -646,7 +646,7 @@ void dance_minus_reset(tap_dance_state_t *state, void *user_data) {
       unregister_code(KC_EQUAL);
       break;
     case TD_DOUBLE_HOLD:
-      unregister_code(KC_PLUS);
+      unregister_code(KC_PPLS);
       break;
   }
   TD_MINUS_tap_state.state = TD_NONE;
