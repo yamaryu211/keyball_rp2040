9a72175e90e4c8c8c1e4b99b6332e46298fc9aa5
yamaryu211 <yamaryu380@gmail.com>
2025-02-24 20:45:55 +0800

    mod: PERMISSIVE_HOLDを有効化

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yamaryu211/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000 // マウスレイヤーを維持する時間を30秒に設定
 
 #define TAPPING_TERM 220
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 
 // タップ→ホールドしたときにタップしたキーコードを連続送信するための待受時間
 #define QUICK_TAP_TERM 150
